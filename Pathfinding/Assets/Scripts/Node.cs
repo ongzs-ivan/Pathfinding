d72e954a4ed315f33dcaefca4591657bbe3db51f
Ivan Ong <pretzelweerum@hotmail.com>
2020-03-02 18:24:16 +0800

    Initial commit

---
+++ Pathfinding/Assets/Scripts/Node.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    private float weight = int.MaxValue;
+    private Transform parentNode = null;
+    private List<Transform> neighbourNode;
+    private bool isWalkable = true;
+
+    void Start()
+    {
+        this.resetNode();
+    }
+
+    public void ResetNode()
+    {
+        weight = int.MaxValue;
+        parentNode = null;
+    }
+}
